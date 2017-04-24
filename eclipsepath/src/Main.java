871c5f313b7362da7fffa1d648075df88a702203
Jan Pfenning <jan@frank-pfenning.de>
2017-04-24 23:58:30 +0200

    test2
    test2

--- eclipsepath/src/Main.java
+++ eclipsepath/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 	
 	public static void main(String[] args){
 		System.out.println("Was richtig innovatives");
-		System.out.println("Test Leon ist dick");
+		System.out.println("Test Leon ist dicker");
 		//(null, "Hi");
 	}
 }
