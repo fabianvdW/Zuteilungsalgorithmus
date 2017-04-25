41993528ce74d9ce2478bf8b62246f6d1d927a1c
Agent77326 <agent77326@gmail.com>
2017-04-25 14:21:25 +0200

    DB fix connection (#18)
    * Change to protected
    
    This class should only be accessed by DB-manager
    
    * Creation of DBManager

--- eclipsepath/src/db/DBManager.java
+++ eclipsepath/src/db/DBManager.java
@@ -1,7 +1,19 @@
 package db;
+
 public class DBManager {
-	public static void initializeJavaObjectsFromDb(){
+	
+	/**
+	 * @author Agent77326
+	 */
+	public DBManager(){
 		
 	}
 	
+	public DB connect(String server, int port, String user, String password, String database){
+		return new DB(server, port, user, password, database);
+	}
+  
+	public static void initializeJavaObjectsFromDb(){
+		
+	}
 }
