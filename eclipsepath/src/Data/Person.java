56e1b60c94f1924b07efc9f880317958e46fdfb8
fabianvdW <fabianvonderwarth@gmail.com>
2017-04-25 13:59:20 +0200

    Verteilungsalgorithmus,AG,DBMAnager, Javadoc, UML
    Verteilungsalgorithmus:
    Add Method checkScore
    Add Method macheAusgabe
    Update Method Main
    
    AG:
    Add Method removeTeilnehmer
    DBManager:
    Method init change to static
    
    Javadoc:
    Updated
    
    UML:
    Updated

--- eclipsepath/src/Data/Person.java
+++ eclipsepath/src/Data/Person.java
@@ -5,15 +5,43 @@ import java.util.ArrayList;
 public class Person {
 	private String name;
 	private ArrayList<Rating> ratings;
+	private AG besuchtAG;
+	/**
+	 * Objekt zum speichern einer Person
+	 * @param name Der Name der Person
+	 * @param ratings Seine AGs bewertet, 0.Element die beste AG, 1.Element die zweit beste usw.
+	 */
 	public Person(String name, ArrayList<Rating> ratings){
 		this.name=name;
 		this.ratings=ratings;
 		ratings= new ArrayList<Rating>();
 	}
+	/**
+	 * 
+	 * @return Name
+	 */
 	public String getName(){
 		return this.name;
 	}
+	/**
+	 * 
+	 * @return Das Rating
+	 */
 	public ArrayList<Rating> getRating(){
 		return this.ratings;
+	}
+	/**
+	 * 
+	 * @param ag Wird der AG zugewiesen
+	 */
+	public void teileAGZu(AG ag){
+		this.besuchtAG=ag;
+	}
+	/**
+	 * 
+	 * @return Return die aG, die die Person besucht.
+	 */
+	public AG getBesuchteAG(){
+		return this.besuchtAG;
 	}
 }
--- eclipsepath/src/db/DBManager.java
+++ eclipsepath/src/db/DBManager.java
@@ -1,6 +1,6 @@
 package db;
 public class DBManager {
-	public void initializeJavaObjectsFromDb(){
+	public static void initializeJavaObjectsFromDb(){
 		
 	}
 	
