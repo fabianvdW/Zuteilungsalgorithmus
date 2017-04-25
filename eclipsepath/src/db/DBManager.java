841092e7d0fb21ea462157f0cd426cd450917842
Agent77326 <agent77326@gmail.com>
2017-04-25 14:46:11 +0200

    Make DB protected (#20)
    * Change to protected
    
    This class should only be accessed by DB-manager
    
    * Creation of DBManager
    
    * little changes

--- eclipsepath/src/db/DBManager.java
+++ eclipsepath/src/db/DBManager.java
@@ -9,10 +9,19 @@ public class DBManager {
 		
 	}
 	
+	/**
+	 * Get a DB-handle to edit the tables in the db
+	 * @param String server
+	 * @param int port
+	 * @param String user
+	 * @param String password
+	 * @param String database
+	 * @return DB (the handle)
+	 */
 	public DB connect(String server, int port, String user, String password, String database){
 		return new DB(server, port, user, password, database);
 	}
-  
+	
 	public static void initializeJavaObjectsFromDb(){
 		
 	}
