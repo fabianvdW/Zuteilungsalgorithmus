5b732105ba564d1af13b3067a30df35ab6703949
Agent77326 <agent77326@gmail.com>
2017-04-25 10:43:45 +0200

    Basic SQL-Interface (#12)
    * Basic SQL-Interface
    
    Adding Interaction with SQL, please DON'T delete this branch!
    
    * Update .classpath

---
+++ eclipsepath/src/db/DB.java
@@ -0,0 +1,159 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DB {
+	private Connection con;
+
+	/**
+	 * Class for managed connections and interactions with a given server
+	 * @author Agent77326
+	 * @param String the server-address e.g: example.com
+	 * @param int the server-port mySQL uses: default is 3306
+	 * @param String the user to login
+	 * @param String the password to authenticate
+	 * @param String the database to connect to
+	 */
+	public DB(String server, int port, String user, String password, String database){
+		try{
+			con = DriverManager.getConnection("jdbc:mysql://localhost/" + database, user, password);
+		}
+		catch(SQLException e){
+			Logger lgr = Logger.getLogger(DB.class.getName());
+			lgr.log(Level.WARNING, e.getMessage(), e);
+		}
+	}
+	
+	/**
+	 * Use this for updating, inserting and deleting data, if you only want to read use query();
+	 * @param String sql the statement with wildcards to fill with the data
+	 * 		e.g: "insert into  database.table values (default, ?, ?, ?, ?, ?, ?)"
+	 * @param String[] data the values here will be inserted into the wildcards at the sql-statement
+	 * @return
+	 */
+	private void update(String sql, String[] data){
+		PreparedStatement ps = null;
+		try{
+			ps = con.prepareStatement(sql);
+			for(int i = 0; i < data.length; i++){
+				ps.setString(i + 1, data[i]);
+			}
+			ps.executeUpdate();
+		}
+		catch(SQLException e){
+			Logger lgr = Logger.getLogger(DB.class.getName());
+			lgr.log(Level.WARNING, e.getMessage(), e);
+		}
+		finally{
+			try{
+				if(ps != null){
+					ps.close();
+	            }
+	        }
+			catch(SQLException e){
+				Logger lgr = Logger.getLogger(DB.class.getName());
+				lgr.log(Level.SEVERE, e.getMessage(), e);
+	        }
+		}
+	}
+	
+	/**
+	 * Generates a request to the server by executing the given query
+	 * @param String query the request as sql-statement
+	 * @return ResultSet the returned data
+	 */
+	private ResultSet query(String sql){
+		try{
+			Statement st = con.createStatement();
+			ResultSet tmp = st.executeQuery(sql);
+			try{
+				st.close();
+			}
+			catch(SQLException e){
+				Logger lgr = Logger.getLogger(DB.class.getName());
+				lgr.log(Level.SEVERE, e.getMessage(), e);
+			}
+			return tmp;
+		}
+		catch(SQLException e){
+			Logger lgr = Logger.getLogger(DB.class.getName());
+			lgr.log(Level.WARNING, e.getMessage(), e);
+		}
+		return null;
+	}
+	
+	/**
+	 * Generates a request to the server by executing the given query
+	 * @param PreparedStatment query the request given as prepared
+	 * @return ResultSet the returned data
+	 */
+	private ResultSet query(PreparedStatement sql){
+		try{
+			ResultSet tmp = sql.executeQuery();
+			try{
+				sql.close();
+			}
+			catch(SQLException e){
+				Logger lgr = Logger.getLogger(DB.class.getName());
+				lgr.log(Level.SEVERE, e.getMessage(), e);
+			}
+			return tmp;
+		}
+		catch(SQLException e){
+			Logger lgr = Logger.getLogger(DB.class.getName());
+			lgr.log(Level.WARNING, e.getMessage(), e);
+		}
+		return null;
+	}
+	
+	/**
+	 * Fetches the given data from a ResultSet into a 2-dimensional Array of Strings.
+	 * IMPORTANT! the first row is filled with the names of the columns
+	 * @param ResultSet the return of a query to the sql-server
+	 * @return String[][] with all data. 1.dim is each row and 2.dim is each column
+	 */
+	private String[][] fetch(ResultSet rs){
+		String[][] data = null;
+		try {
+			int i = 0;
+			do{
+				i++;
+			}
+			while(rs.next());
+			rs.first();
+			int n = rs.getMetaData().getColumnCount();
+			data = new String[i + 1][n];
+			for(int j = 0; j < n; j++){
+				data[0][j] = rs.getMetaData().getColumnName(j + 1);
+			}
+			i = 1;
+			do{
+				for(int j = 0; j < n; j++){
+					data[i][j] = rs.getString(j + 1);
+				}
+			}
+			while(rs.next());
+		}
+		catch (SQLException e){
+			Logger lgr = Logger.getLogger(DB.class.getName());
+			lgr.log(Level.WARNING, e.getMessage(), e);
+		}
+		finally {
+			try{
+				rs.close();
+			}
+			catch(SQLException e){
+				Logger lgr = Logger.getLogger(DB.class.getName());
+				lgr.log(Level.SEVERE, e.getMessage(), e);
+			}
+		}
+		return data;
+	}
+}
